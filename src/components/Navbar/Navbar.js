8a52be80e93f37692afd28b022f3287c30c96cd3
Sakib12345 <sakibalislam12345@gmail.com>
2021-05-02 21:52:00 +0600

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 import { NavLinks } from './NavbarElement';
 import { FaBars } from 'react-icons/fa';
